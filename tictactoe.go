7b874b0a3255edfb22e33bb5232f978c514e2c5b
Aaron O. Ellis <aaronoellis@gmail.com>
2015-12-30 18:23:16 -0700

    Bot that should play a perfect game

--- tictactoe.go
+++ tictactoe.go
@@ -5,7 +5,122 @@ import (
 	"strconv"
 )
 
+type Endgame struct {
+	X, O, Tied int
+}
+
+func (end Endgame) String() string {
+	var total = float64(end.X + end.O + end.Tied)
+	return fmt.Sprintf(
+		"%.1f%% of X winning, %.1f%% of O winning, %.1f%% of a tie\n",
+		float64(end.X)/total*100,
+		float64(end.O)/total*100,
+		float64(end.Tied)/total*100,
+	)
+}
+
+// Create a tree structure of all possibilities
+type Play struct {
+	board   Board
+	choices []Play
+	end     Endgame
+}
+
+func (play *Play) descend(player Player) {
+	// Determine the available moves
+	options := play.board.available()
+
+	// Will the player win if the make any of these moves?
+	// TODO multiple winning moves?
+	for _, option := range options {
+		// Create a copy of the board
+		board := play.board.Copy()
+
+		board[option] = player
+		if board.wonBy(player) {
+			var end Endgame
+			if player == X {
+				end.X = 1
+			} else {
+				end.O = 1
+			}
+
+			// Choose this as the sole play
+			play.choices = []Play{{board: board, end: end}}
+			return
+		}
+	}
+
+	// Otherwise play every possible move
+	play.choices = make([]Play, len(options))
+
+	for i, option := range options {
+		board := play.board.Copy()
+		board[option] = player
+		play.choices[i].board = board
+
+		// If these was the last possible move, record it as a tie
+		if len(options) == 1 {
+			play.choices[i].end.Tied = 1
+		}
+	}
+	return
+}
+
+// Save the plays by board
+var choices = make(map[Board]Play)
+
+func descend(play Play, turn int) Play {
+	var player Player
+	if turn%2 == 0 {
+		player = X
+	} else {
+		player = O
+	}
+
+	// TODO call this play?
+	play.descend(player)
+
+	// TODO check if the board already exists!
+
+	for i, choice := range play.choices {
+		// Only descend if the game isn't over
+		if choice.end.X == 0 && choice.end.O == 0 && choice.end.Tied == 0 {
+			// TODO Or just mutate
+			play.choices[i] = descend(choice, turn+1)
+		}
+
+		// Aggregate the endgames
+		play.end.X += play.choices[i].end.X
+		play.end.O += play.choices[i].end.O
+		play.end.Tied += play.choices[i].end.Tied
+	}
+
+	choices[play.board] = play
+	return play
+}
+
+// Recursively construct all game boards
+func buildEndgames() {
+	// Create the initial move then descend
+	var play Play
+	play = descend(play, 0)
+
+	// Count all the endgames
+	// fmt.Println("X Won:", play.end.X)
+	// fmt.Println("Y Won:", play.end.Y)
+	// fmt.Println("Tied:", play.end.Tied)
+
+	// fmt.Println("Boards:", len(choices))
+
+	return
+}
+
 type Board [9]*int
+
+func (b Board) Copy() Board {
+	return b
+}
 
 func (b Board) String() (out string) {
 	for i, square := range b {
@@ -114,8 +229,9 @@ func (board *Board) turn(current Player) {
 }
 
 func main() {
-	var board Board
+	buildEndgames()
 
+	var board Board
 	var turn int
 	for {
 		// Are there available squares?
@@ -125,15 +241,37 @@ func main() {
 			return
 		}
 
+		endgame := choices[board]
+
 		var current Player
 		if turn%2 == 0 {
 			current = X
+			board.turn(current)
 		} else {
 			current = O
+
+			// Have the computer pick a play
+			// Pick the play with the highest chance of O winning
+			var highest float64
+			var choice int
+
+			for i, chances := range endgame.choices {
+				total := chances.end.O + chances.end.X + chances.end.Tied
+				// Go for highest win
+				win := float64(chances.end.O) / float64(total)
+				if win > highest {
+					highest = win
+					choice = i
+				}
+
+			}
+			board = endgame.choices[choice].board
 		}
 
-		board.turn(current)
 		fmt.Println(board)
+
+		// Show the win probability
+		fmt.Println(endgame.end)
 
 		if board.wonBy(current) {
 			fmt.Printf("Player %c has won\n", *current)
